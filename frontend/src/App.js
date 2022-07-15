8807bec3f37c0266a0a52c76d689ebf962f35601
Navruz <navuz.ochilov98@mail.ru>
2022-07-15 10:00:40 +0300

    route-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import { Suspense } from 'react';
+import { Login } from './Pages/Login/Login';
 import PageRoutes from './Pages/PageRoutes';
 
 function App() {
+  const localdata = JSON.parse(localStorage.getItem('userData'));
+  const isAuthenticated = localdata ? !!localdata.token : null;
+
   return (
     <div className='App'>
       <Suspense fallback={'Loading'}>
-        <PageRoutes />
+        {isAuthenticated ? <PageRoutes /> : <Login />}
       </Suspense>
     </div>
   );
--- frontend/src/Pages/PageRoutes.js
+++ frontend/src/Pages/PageRoutes.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Login } from './Login/Login';
 import { MainPage } from './MainPage/MainPage';
 
 const PageRoutes = () => {
-  const localdata = JSON.parse(localStorage.getItem('userData'));
-  const isAuthenticated = localdata ? !!localdata.token : null;
-
-  if (isAuthenticated) {
-    return (
-      <Routes>
-        <Route path='/' index element={<MainPage />} />
-      </Routes>
-    );
-  }
   return (
     <Routes>
-      <Route path='/' index element={<Login />} />
-      <Route path='/administration' element={<h1>Admin panel</h1>} />
+      <Route path='/' index element={<MainPage />} />
     </Routes>
   );
 };
