9fe1518e6843e4d197b8343404a94349300198f0
Navruz <navuz.ochilov98@mail.ru>
2022-07-15 15:52:52 +0300

    pagination done

--- frontend/src/Pages/MainPage/MainPage.js
+++ frontend/src/Pages/MainPage/MainPage.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Pagination } from '../../Components/Pagination/Pagination';
 
 export const MainPage = () => {
-  return <div>MainPage</div>;
+  const [currentPage, setCurrentPage] = useState(0);
+  return (
+    <>
+      <div className='m-3 flex justify-center'>
+        <Pagination
+          countPage={10}
+          totalDatas={2300}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
+      </div>
+    </>
+  );
 };
