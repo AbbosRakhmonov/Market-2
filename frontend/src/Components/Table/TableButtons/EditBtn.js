767ce4bcbb6ff908fbca46712caf6acaf87ec49c
Navruz <navuz.ochilov98@mail.ru>
2022-07-16 10:45:49 +0300

    table

---
+++ frontend/src/Components/Table/TableButtons/EditBtn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { IoCreateOutline } from 'react-icons/io5';
+
+export const EditBtn = () => {
+  return (
+    <div className='w-[24px] h-[24px] bg-warning-500 rounded-full flex justify-center items-center'>
+      <IoCreateOutline color='white' />
+    </div>
+  );
+};
--- frontend/src/Components/Table/Thead.js
+++ frontend/src/Components/Table/Thead.js
@@ -1,12 +1,13 @@
-function Thead({headers}) {
-    return (
-        <tr>
-            {
-                headers.map(header => header.filter ? "" :
-                    <th className={`col-span-${header.colSpan}`}>{header.title}</th>)
-            }
-        </tr>
-    );
+function Thead({ headers }) {
+  return (
+    <tr className='grid grid-cols-12'>
+      {headers.map((header) => (
+        <th className={`col-span-${header.colSpan} bg-primary-900`}>
+          {header.title}
+        </th>
+      ))}
+    </tr>
+  );
 }
 
 export default Thead;
