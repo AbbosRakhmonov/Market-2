e4c3b950c685168ad3f94b0797876896a6ee83f6
Navruz <navuz.ochilov98@mail.ru>
2022-07-19 14:03:25 +0300

    product table debug

--- frontend/src/Components/Table/TableRows/ProductTableRow.js
+++ frontend/src/Components/Table/TableRows/ProductTableRow.js
@@ -10,44 +10,38 @@ export const ProductTableRow = ({
 }) => {
   return (
     <>
-      {data.map((product, index) => {
-        return (
-          <tr key={index} className='tr '>
-            <td className='td text-center '>
-              {currentPage * countPage + 1 + index}
-            </td>
-            <td className='td text-center'>{product.productdata.code}</td>
-            <td className='td text-left'>{product.productdata.name}</td>
-            <td className='td text-right'>
-              {product.total} {product.unit && product.unit.name}
-            </td>
-            <td className='td text-right'>
-              {product.price &&
-                product.price.incomingprice.toLocaleString('ru-RU')}{' '}
-              USD
-            </td>
-            <td className='td text-right'>
-              {product.price &&
-                product.price.sellingprice.toLocaleString('ru-RU')}{' '}
-              USD
-            </td>
-            <td className='td border-r-0 text-center max-w-[50px]'>
-              <div className='flex items-center justify-center'>
-                <TableBtn
-                  type={'edit'}
-                  bgcolor='bg-warning-500'
-                  onClick={Edit}
-                />
-                <TableBtn
-                  type={'delete'}
-                  bgcolor='bg-error-500 ml-2.5'
-                  onClick={Delete}
-                />
-              </div>
-            </td>
-          </tr>
-        );
-      })}
+      {data.map((product, index) => (
+        <tr key={product._id} className='tr '>
+          <td className='td text-center '>
+            {currentPage * countPage + 1 + index}
+          </td>
+          <td className='td text-center'>{product.productdata.code}</td>
+          <td className='td text-left'>{product.productdata.name}</td>
+          <td className='td text-right'>
+            {product.total} {product.unit && product.unit.name}
+          </td>
+          <td className='td text-right'>
+            {product.price &&
+              product.price.incomingprice.toLocaleString('ru-RU')}{' '}
+            USD
+          </td>
+          <td className='td text-right'>
+            {product.price &&
+              product.price.sellingprice.toLocaleString('ru-RU')}{' '}
+            USD
+          </td>
+          <td className='td border-r-0 text-center max-w-[50px]'>
+            <div className='flex items-center justify-center'>
+              <TableBtn type={'edit'} bgcolor='bg-warning-500' onClick={Edit} />
+              <TableBtn
+                type={'delete'}
+                bgcolor='bg-error-500 ml-2.5'
+                onClick={Delete}
+              />
+            </div>
+          </td>
+        </tr>
+      ))}
     </>
   );
 };
