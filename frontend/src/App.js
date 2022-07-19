0cdd4539679ae9a0dc5d000619d434b23450b082
Navruz <navuz.ochilov98@mail.ru>
2022-07-19 09:40:53 +0300

    product table done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='App'>
       <Suspense fallback={'Loading'}>
-        {true ? <PageRoutes /> : <Login />}
+        {logged ? <PageRoutes /> : <Login />}
       </Suspense>
     </div>
   );
