d77043220613eedad0a6347f32734fc9ac7a01ab
Navruz <navuz.ochilov98@mail.ru>
2022-07-20 17:08:54 +0300

    page

--- frontend/src/Pages/MainPage/MainPage.js
+++ frontend/src/Pages/MainPage/MainPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Pagination } from '../../Components/Pagination/Pagination';
 
 function MainPage(props) {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <Pagination
+        countPage={10}
+        totalDatas={1000}
+        setCurrentPage={() => {}}
+        currentPage={0}
+      />
+    </div>
+  );
 }
 
 export default MainPage;
