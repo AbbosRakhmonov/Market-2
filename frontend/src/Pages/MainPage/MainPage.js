e28c61472b216ca930115c568cd231dd5e7fdcdc
Navruz <navuz.ochilov98@mail.ru>
2022-07-21 10:20:30 +0300

    table done 3

--- frontend/src/Pages/MainPage/MainPage.js
+++ frontend/src/Pages/MainPage/MainPage.js
@@ -2,54 +2,7 @@ import React from 'react';
 import Table from '../../Components/Table/Table';
 
 function MainPage(props) {
-  const headers = [
-    {
-      title: '№',
-      filter: '',
-    },
-    {
-      title: 'Kategoriya kodi',
-      filter: 'code',
-    },
-    {
-      title: 'Kategoriya nomi',
-      filter: 'name',
-    },
-    {
-      title: '',
-      filter: '',
-    },
-  ];
-
-  const data = [
-    {
-      _id: 1,
-      name: 'Truba',
-      code: '001001',
-    },
-    {
-      _id: 2,
-      name: 'Truba',
-      code: '001001',
-    },
-    {
-      _id: 3,
-      name: 'Truba',
-      code: '001001',
-    },
-  ];
-
-  return (
-    <div>
-      <Table
-        page={'category'}
-        headers={headers}
-        data={data}
-        currentPage={0}
-        countPage={10}
-      />
-    </div>
-  );
+  return <div>MainPage</div>;
 }
 
 export default MainPage;
