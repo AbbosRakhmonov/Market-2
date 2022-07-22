23e1fe9e3466063edc72b1d94a53315ff019be2f
Umidjon8787 <100425651+Umidjon8787@users.noreply.github.com>
2022-07-22 18:18:38 +0300

    Update MainPage.js
    main page

--- frontend/src/Pages/MainPage/MainPage.js
+++ frontend/src/Pages/MainPage/MainPage.js
@@ -1,32 +1,7 @@
 import React from 'react';
-import {
-  DiscountBtn,
-  Payment,
-  PaymentClip,
-  SaleBtn,
-} from '../../Components/Buttons/SaleBtns';
 
-function MainPage(props) {
-  return (
-    <>
-      <div className='grid grid-cols-2 gap-2 w-[290px]'>
-        <>
-          <SaleBtn text={'Naqd'} type={"cash"} />
-          <SaleBtn text={'Plastik'} type={"card"}/>
-        </>
-        <>
-          <SaleBtn text={"O'tkazma"} type={"transfer"} />
-          <SaleBtn text={'Aralash'} type={"mixed"}/>
-        </>
-
-        <DiscountBtn text={'Chegirma'} />
-      </div>
-      <div className='flex justify-between w-[290px]'>
-        <Payment text={"To'lash"} />
-        <PaymentClip/>
-      </div>
-    </>
-  );
+function MainPage() {
+  return <div>Bosh sahifa</div>;
 }
 
 export default MainPage;
