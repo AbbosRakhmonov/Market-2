ca4b92a25dd3ebc1ef554833db7d0a6b534a7d6c
Navruz <navuz.ochilov98@mail.ru>
2022-07-22 10:32:16 +0300

    table done

--- frontend/src/Pages/MainPage/MainPage.js
+++ frontend/src/Pages/MainPage/MainPage.js
@@ -1,7 +1,87 @@
 import React from 'react';
+import Table from '../../Components/Table/Table';
 
 function MainPage(props) {
-  return <div>MainPage</div>;
+  const headers = [
+    {
+      title: '№',
+      filter: '',
+    },
+    {
+      title: 'Kodi',
+      filter: 'name',
+    },
+    {
+      title: 'Nomi',
+      filter: 'code',
+    },
+    {
+      title: 'Dastlabki',
+      filter: '',
+    },
+    {
+      title: 'Sanoq',
+      filter: '',
+    },
+    {
+      title: 'Farq',
+      filter: '',
+    },
+    {
+      title: 'Izoh',
+      filter: '',
+    },
+    {
+      title: '',
+    },
+  ];
+
+  const data = [
+    {
+      productdata: {
+        name: 'Truba',
+        code: '002002',
+      },
+      unit: {
+        name: 'dona',
+      },
+      total: 100,
+      difference: 99,
+    },
+    {
+      productdata: {
+        name: 'Truba',
+        code: '002002',
+      },
+      unit: {
+        name: 'dona',
+      },
+      total: 100,
+      difference: 99,
+    },
+    {
+      productdata: {
+        name: 'Truba',
+        code: '002002',
+      },
+      unit: {
+        name: 'dona',
+      },
+      total: 100,
+      difference: 99,
+    },
+  ];
+
+  return (
+    <div>
+      <Table
+        page={'inventory'}
+        headers={headers}
+        data={data}
+        currency={'USD'}
+      />
+    </div>
+  );
 }
 
 export default MainPage;
