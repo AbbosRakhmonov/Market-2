316225cee14e34667eaadfcd114dfa4766b50ba4
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2022-07-23 09:06:07 +0500

    Edited index.css

--- frontend/src/Components/Inputs/SearchInput.js
+++ frontend/src/Components/Inputs/SearchInput.js
@@ -6,7 +6,7 @@ function SearchInput({placeholder, value, onChange, someClasses}) {
             className={`searchInput-container shadow-[0_10px_10px_rgba(0,0,0,0.05)] flex items-center ${someClasses ? someClasses : ""}`}>
             <IoSearchOutline className='searchIcon text-[1.31rem] text-blue-200'/>
             <input
-                className='leading-[1.125rem] transition-all ease-in-out duration-100 px-[8px] py-[0.625rem] w-full bg-transparent outline-none'
+                className='leading-[1.125rem] placeholder-blue-200 transition-all ease-in-out duration-100 px-[8px] py-[0.625rem] w-full bg-transparent outline-none'
                 placeholder={placeholder}
                 type={"search"}
                 value={value}
