1f1b6718a20184039860613ae0472d52f9454454
Umidjon8787 <100425651+Umidjon8787@users.noreply.github.com>
2022-07-25 16:12:53 +0300

    correction

--- frontend/src/Pages/MainPage/MainPage.js
+++ frontend/src/Pages/MainPage/MainPage.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { ExitBtn } from '../../Components/Buttons/ExitBtn';
 import { DiscountInput } from '../../Components/Inputs/DiscountInputs';
 
 function MainPage() {
-  return <div>
-    <DiscountInput/>
-    <ExitBtn/>
-  </div>;
+  return <div>Main Page</div>;
 }
 
 export default MainPage;
