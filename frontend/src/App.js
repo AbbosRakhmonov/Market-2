9f76b532e40ceb409ab8ab2d6837cb35ebcba885
abbosrakhmonov <abbos.raxmonov.2001@gmail.com>
2022-07-25 11:16:16 +0500

    confiktlar tugirlandi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,27 @@
-import {lazy, Suspense, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {logIn} from './Pages/Login/loginSlice';
-import {getCurrency, getCurrencyType} from "./Pages/Currency/currencySlice";
+import { lazy, Suspense, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logIn } from './Pages/Login/loginSlice';
 
 // pages
 const Login = lazy(() => import('./Pages/Login/Login'));
 const PageRoutes = lazy(() => import('./Pages/PageRoutes'));
 
 function App() {
-    const dispatch = useDispatch();
-    const {logged} = useSelector((state) => state.login);
-    useEffect(() => {
-        const userData = JSON.parse(localStorage.getItem('userData'));
-        if (userData) {
-            dispatch(logIn(userData));
-            dispatch(getCurrency());
-            dispatch(getCurrencyType());
-        }
-    }, [dispatch]);
-    return (
-        <div className='App'>
-            <Suspense fallback={'loading'}>
-                {logged ? <PageRoutes/> : <Login/>}
-            </Suspense>
-        </div>
-    );
+  const dispatch = useDispatch();
+  const { logged } = useSelector((state) => state.login);
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    if (userData) {
+      dispatch(logIn(userData));
+    }
+  }, [dispatch]);
+  return (
+    <div className='App'>
+      <Suspense fallback={'loading'}>
+        {logged ? <PageRoutes /> : <Login />}
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/Pages/Currency/currencySlice.js
+++ frontend/src/Pages/Currency/currencySlice.js
@@ -1,67 +1,73 @@
-import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
-import Api from "../../Config/Api";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import Api from '../../Config/Api';
 
 export const getCurrency = createAsyncThunk(
-    "currency/getCurrency",
-    async (body, {rejectWithValue}) => {
-        try {
-            const {data:{exchangerate}} = await Api.post("/exchangerate/get");
-            return exchangerate;
-        } catch (error) {
-            return rejectWithValue(error);
-        }
-    });
+  'currency/getCurrency',
+  async (body, { rejectWithValue }) => {
+    try {
+      const {
+        data: { exchangerate },
+      } = await Api.post('/exchangerate/get');
+      return exchangerate;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 export const getCurrencyType = createAsyncThunk(
-    "currency/getCurrencyType",
-    async (body, {rejectWithValue}) => {
-        try {
-            const {data: {currency}} = await Api.post("/exchangerate/currencyget");
-            return currency;
-        } catch (error) {
-            return rejectWithValue(error);
-        }
-    })
+  'currency/getCurrencyType',
+  async (body, { rejectWithValue }) => {
+    try {
+      const {
+        data: { currency },
+      } = await Api.post('/exchangerate/currencyget');
+      return currency;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 const currencySlice = createSlice({
-    name: "currency",
-    initialState: {
-        currency: null,
-        currencies: [],
-        currencyType: "",
-        loading: false,
-        error: null,
+  name: 'currency',
+  initialState: {
+    currency: null,
+    currencies: [],
+    currencyType: '',
+    loading: false,
+    currencyError: null,
+  },
+  reducers: {
+    clearError: (state) => {
+      state.currencyError = null;
     },
-    reducers: {
-        clearError: (state) => {
-            state.error = null;
-        },
+  },
+  extraReducers: {
+    [getCurrency.pending]: (state) => {
+      state.loading = true;
     },
-    extraReducers: {
-        [getCurrency.pending]: (state) => {
-            state.loading = true;
-        },
-        [getCurrency.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.currency = action.payload;
-        },
-        [getCurrency.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-        },
-        [getCurrencyType.pending]: (state) => {
-            state.loading = true;
-        },
-        [getCurrencyType.fulfilled]: (state, action) => {
-            state.loading = false;
-            state.currencyType = action.payload;
-        },
-        [getCurrencyType.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-        }
+    [getCurrency.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.currency = action.payload;
     },
+    [getCurrency.rejected]: (state, action) => {
+      state.loading = false;
+      state.currencyError = action.payload;
+    },
+    [getCurrencyType.pending]: (state) => {
+      state.loading = true;
+    },
+    [getCurrencyType.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.currencyType = action.payload;
+    },
+    [getCurrencyType.rejected]: (state, action) => {
+      state.loading = false;
+      state.currencyError = action.payload;
+    },
+  },
 });
 
-export const {clearError} = currencySlice.actions;
+export const { clearError } = currencySlice.actions;
 export default currencySlice.reducer;
