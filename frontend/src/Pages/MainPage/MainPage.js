a71602802d89ad1db87f145eaf486571713f6c61
Navruz <navuz.ochilov98@mail.ru>
2022-07-26 15:10:19 +0300

    filter-icon done

--- frontend/src/Pages/MainPage/MainPage.js
+++ frontend/src/Pages/MainPage/MainPage.js
@@ -1,44 +1,6 @@
 import React, { useState } from 'react';
-import Thead from '../../Components/Table/Thead';
 
 function MainPage() {
-  const headers = [
-    {
-      title: '№',
-      filter: '',
-    },
-    {
-      title: 'Nomi',
-      filter: 'name',
-    },
-    {
-      title: 'Kodi',
-      filter: 'code',
-    },
-    {
-      title: 'Soni',
-      filter: 'total',
-    },
-  ];
-
-  const [sortItem, setSortItem] = useState({
-    filter: '',
-    sort: '',
-  });
-
-  return (
-    <div>
-      <Thead
-        headers={headers}
-        Sort={(filter) =>
-          setSortItem({
-            filter: filter,
-            sort: '0',
-          })
-        }
-        sortItem={sortItem}
-      />
-    </div>
-  );
+  return <div>MainPage</div>;
 }
 export default MainPage;
