84d06229d9d46db34f978d2d7191f25ec9fec0d6
Dilsuz07 <dilsuz25012303@gmail.com>
2022-07-27 16:40:07 +0500

    k

--- frontend/src/Pages/MainPage/MainPage.js
+++ frontend/src/Pages/MainPage/MainPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import SelectForm from '../../Components/Select/SelectForm';
 
 function MainPage() {
-  return <div>MainPage</div>;
+  return <div><SelectForm /></div>;
 }
 export default MainPage;
