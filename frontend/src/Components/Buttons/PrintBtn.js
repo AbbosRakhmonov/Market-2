bd8574b8ecbc6a9bd9d996435af88e6d4ef77b54
Jasurbek Toshev <jasurbektoshev9@gmail.com>
2022-07-28 17:19:21 +0500

    New prind btn branch

---
+++ frontend/src/Components/Buttons/PrintBtn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { IoPrint } from "react-icons/io5";
+function PrintBtn({onClick}){
+    return(
+        <section>
+            <button className="print-btn-style" onClick={onClick}>
+                <span className="print-text-style">Chop etish</span>
+                <span className="print-icon-style"><IoPrint className="w-[0.9rem] text-primary-800 h-[0.9rem]"/></span>
+            </button>
+        </section>
+    )
+}
+
+export default PrintBtn; 
