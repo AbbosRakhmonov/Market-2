4da6ab3aafc34772e29c21fb4bad45595775f762
Navruz <navuz.ochilov98@mail.ru>
2022-08-16 14:21:56 +0300

    done

--- server/routers/charts/charts.js
+++ server/routers/charts/charts.js
@@ -2,7 +2,10 @@ const { Market } = require('../../models/MarketAndBranch/Market');
 const { Incoming } = require('../../models/Products/Incoming');
 const { SaleProduct } = require('../../models/Sales/SaleProduct');
 const { SaleConnector } = require('../../models/Sales/SaleConnector');
+const { Expense } = require('../../models/Expense/Expense');
 require('../../models/Sales/Payment');
+require('../../models/Sales/Discount');
+require('../../models/Products/ProductPrice');
 
 module.exports.getIncomingData = async (req, res) => {
   try {
@@ -113,7 +116,86 @@ module.exports.getSalesDataByMonth = async (req, res) => {
       count += 1;
     }
 
-    res.status(200).json({ sales, salesSum });
+    // Oylik soffoyda chiqarish
+    const monthProfit = {
+      usd: 0,
+      uzs: 0,
+    };
+
+    const saleconnectors = await SaleConnector.find({
+      market,
+      createdAt: {
+        $gte: new Date(new Date().setDate(1)).toISOString(),
+        $lte: new Date().toISOString(),
+      },
+    })
+      .select('payments discounts products')
+      .populate('payments', 'totalprice totalpriceuzs')
+      .populate({
+        path: 'products',
+        select: 'pieces price',
+        populate: {
+          path: 'price',
+          select: 'incomingprice incomingpriceuzs',
+        },
+      })
+      .populate('discounts', 'discount discountuzs');
+
+    saleconnectors.map((sale) => {
+      const totalprice = sale.payments.reduce((prev, { totalprice }) => {
+        return prev + (totalprice || 0);
+      }, 0);
+      const totalpriceuzs = sale.payments.reduce((prev, { totalpriceuzs }) => {
+        return prev + (totalpriceuzs || 0);
+      }, 0);
+      const incomingtotal = sale.products.reduce((prev, product) => {
+        return prev + product.price.incomingprice * product.pieces;
+      }, 0);
+      const incomingtotaluzs = sale.products.reduce((prev, product) => {
+        return prev + product.price.incomingpriceuzs * product.pieces;
+      }, 0);
+      const discount = sale.discounts.reduce((prev, discount) => {
+        return prev + discount.discount;
+      }, 0);
+      const discountuzs = sale.discounts.reduce((prev, discount) => {
+        return prev + discount.discountuzs;
+      }, 0);
+
+      monthProfit.usd += totalprice - incomingtotal - discount;
+      monthProfit.uzs += totalpriceuzs - incomingtotaluzs - discountuzs;
+    });
+
+    // Xarajat chiqarish bir oylik
+    const monthExpense = {
+      usd: 0,
+      uzs: 0,
+    };
+
+    const expenses = await Expense.find({
+      market,
+      createdAt: {
+        $gte: new Date(new Date().setDate(1)).toISOString(),
+        $lte: new Date().toISOString(),
+      },
+    }).select('sum sumuzs');
+
+    expenses.map((expense) => {
+      monthExpense.usd += expense.sum;
+      monthExpense.uzs += expense.sumuzs;
+    });
+
+    monthProfit.usd = Math.round(monthProfit.usd * 1000) / 1000;
+    monthProfit.uzs = Math.round(monthProfit.uzs * 1) / 1;
+
+    monthExpense.usd = Math.round(monthExpense.usd * 1000) / 1000;
+    monthExpense.uzs = Math.round(monthExpense.uzs * 1) / 1;
+
+    res.status(200).json({
+      sales,
+      salesSum,
+      monthProfit,
+      monthExpense,
+    });
   } catch (error) {
     res.status(501).json({ error: 'Serverda xatolik yuz berdi...' });
   }
