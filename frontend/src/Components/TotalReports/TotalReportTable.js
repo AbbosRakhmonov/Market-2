782c66efea26be94b54e74baef2832c06f28d0df
Navruz <navuz.ochilov98@mail.ru>
2022-08-22 15:49:33 +0300

    done

--- frontend/src/Components/TotalReports/TotalReportTable.js
+++ frontend/src/Components/TotalReports/TotalReportTable.js
@@ -6,26 +6,24 @@ const TotalReportTable = ({number, currency, cost, plastic, transfer}) => {
             <tbody>
                 <tr className='tr-div'>
                     <td className='td-page'>Sotish soni</td>
-                    <td className='td-page-two'>{number}</td>
+                    <td className='td-page-two font-bold'>{number}</td>
                 </tr>
                 <tr className='tr-div'>
                     <td className='td-page'>Naqd</td>
-                    <td className='td-page-two'>
-                        {cost} <span className='font-medium'>{currency}</span>
+                    <td className='td-page-two font-bold'>
+                        {cost} {currency}
                     </td>
                 </tr>
                 <tr className='tr-div'>
                     <td className='td-page'>Plastik</td>
-                    <td className='td-page-two'>
-                        {plastic}{' '}
-                        <span className='font-medium'>{currency}</span>
+                    <td className='td-page-two font-bold'>
+                        {plastic} {currency}
                     </td>
                 </tr>
                 <tr className='tr-div'>
                     <td className='td-page'>O`tkazma</td>
-                    <td className='td-page-two'>
-                        {transfer}{' '}
-                        <span className='font-medium'>{currency}</span>
+                    <td className='td-page-two font-bold'>
+                        {transfer} {currency}
                     </td>
                 </tr>
             </tbody>
