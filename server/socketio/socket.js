1ad185c1418c59e3ae02d07d3d8b8de4ac1759c9
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2022-08-30 18:02:22 +0500

    Socket disebled

--- server/socketio/socket.js
+++ server/socketio/socket.js
@@ -1,16 +1,6 @@
 const { getAllCategories, getAllProducts } = require("./socketFunctions.js");
 const socketIO = (io) => {
-  io.on("connection", (socket) => {
-    socket.on("getAllCategories", async (market) => {
-      const categories = await getAllCategories(market);
-      socket.emit("getAllCategories", categories);
-    });
-
-    socket.on("getAllProducts", async (market) => {
-      const products = await getAllProducts(market);
-      socket.emit("getAllProducts", products);
-    });
-  });
+  io.on("connection", (socket) => {});
 };
 
 module.exports = { socketIO };
