c391acea77307716c2d332fb2708ae186514da3e
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2022-09-13 14:46:26 +0500

    Filials

--- frontend/src/Pages/ShopsPage/Shops.js
+++ frontend/src/Pages/ShopsPage/Shops.js
@@ -3,18 +3,15 @@ import Filial from '../../Components/Filial/Filial'
 import {uniqueId, map} from 'lodash'
 import {motion} from 'framer-motion'
 import {useDispatch, useSelector} from 'react-redux'
-import {getExchangesFilial} from "./../ProductExchanges/productExchangesSlice"
+import {getExchangesFilial} from './../ProductExchanges/productExchangesSlice'
 import SmallLoader from '../../Components/Spinner/SmallLoader'
 function Shops() {
-    
     const dispatch = useDispatch()
     const {filialDatas, loading} = useSelector((state) => state.exchanges)
     const [data, setData] = useState([])
 
-    
-
     useEffect(() => {
-        const newData = filialDatas?.map((item) => {
+        const newData = filialDatas.map((item) => {
             return {
                 director: {
                     firstname: item?.director?.firstname,
@@ -23,14 +20,13 @@ function Shops() {
                 },
                 typecount: 10,
                 productcount: 100,
-                totalPrice:1000000,
-                totalPriceUSD:100,
+                totalPrice: 1000000,
+                totalPriceUSD: 100,
                 shopname: item?.name,
                 _id: item?._id,
             }
         })
         setData(newData)
-       
     }, [filialDatas])
 
     useEffect(() => {
@@ -57,20 +53,17 @@ function Shops() {
             }}
             transition={{duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98]}}
         >
-             {loading &&
-                <div
-                    className='fixed backdrop-blur-[2px] left-0 right-0 bg-white-700 flex flex-col items-center justify-center w-full h-full'>
+            {loading && (
+                <div className='fixed backdrop-blur-[2px] left-0 right-0 bg-white-700 flex flex-col items-center justify-center w-full h-full'>
                     <SmallLoader />
-                </div>}
-            {map(data, (value) => {
-                return (
+                </div>
+            )}
+            {data.length &&
+                map(data, (value) => (
                     <div className='pb-[1.25rem]' key={uniqueId('filial')}>
-                        <Filial
-                            value= {value}        
-                        />
+                        <Filial value={value} />
                     </div>
-                )
-            })}
+                ))}
         </motion.section>
     )
 }
